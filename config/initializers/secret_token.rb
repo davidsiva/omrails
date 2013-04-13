377b4798d538900697fc6a916dca67bd4b1efb62
Davidsiva <david@printcloud.ca>
2013-04-13 19:35:30 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omrails::Application.config.secret_token = '0344d2a21e77545e3ecd249765388e92d0a8902696870d13fbfd2655cb488289bd3e6eb3e1d5950cc58ee6b738a90b099eb46762ee0c23cf4c51aa06d62d4c63'
